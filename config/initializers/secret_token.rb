e2fdd9f2e51333121177a1901a900e6e6f1e34e9
Satya <satya@satya.me>
2014-02-06 22:31:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newnantsocial::Application.config.secret_token = '1f718cfa14b8cfc2d7b0d829e4d24bc27fc3cdc12a267f529b611b7c3fa349c27875fc18264f15fae0425b003c20ded4dddfc925d9e6f9dd40e9bfaabfb372b4'
